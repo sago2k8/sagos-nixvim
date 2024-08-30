2a0c93a641abe96513b1b77ba92eef0755fd45a6
Santiago Jimenez Giraldo <sago2k8@gmail.com>
2024-08-30 16:49:08 +0200

    feat: use neo-tree instead of the default explorer
    Signed-off-by: Santiago Jimenez Giraldo <sago2k8@gmail.com>

--- config/default.nix
+++ config/default.nix
@@ -64,7 +64,6 @@
     ./utils/persistence.nix
     ./utils/plenary.nix
     ./utils/project-nvim.nix
-    ./utils/sidebar.nix
     ./utils/tmux-navigator.nix
     ./utils/todo-comments.nix
     ./utils/toggleterm.nix
